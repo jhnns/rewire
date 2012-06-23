c608ad6c5bbbe5981bb25cff0cbb57940509f244
Johannes <johannes.ewald@roomieplanet.de>
2012-06-23 16:36:05 +0200

    fixed bug when node_modules folder is missing

--- test/testHelpers/createFakePackageJSON.js
+++ test/testHelpers/createFakePackageJSON.js
@@ -5,7 +5,9 @@ var fs = require("fs"),
 module.exports = function createFakePackageJSON(done) {
     var fakePackageJSON = '{ "main": "../../lib/index.js" }';
 
-    fs.mkdir(nodeModulesDir + "/rewire", function onMkDir() {
-        fs.writeFile(nodeModulesDir + "/rewire/package.json", fakePackageJSON, "utf8", done);
+    fs.mkdir(nodeModulesDir, function onMkDirNodeModules() {
+        fs.mkdir(nodeModulesDir + "/rewire", function onRewireMkDir() {
+            fs.writeFile(nodeModulesDir + "/rewire/package.json", fakePackageJSON, "utf8", done);
+        });
     });
 };
