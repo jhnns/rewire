7bec7f88e01bad4829ee2ac3f90fe5d9d2616888
Rens Baardman <git@rensbaardman.nl>
2019-06-11 13:41:59 +0200

    Fix #167: non-enumerable globals are now also prefixed with `var`

--- test/getImportGlobalsSrc.test.js
+++ test/getImportGlobalsSrc.test.js
@@ -3,6 +3,7 @@ var expect = require("expect.js"),
     getImportGlobalsSrc = require("../lib/getImportGlobalsSrc.js");
 
 describe("getImportGlobalsSrc", function () {
+
     it("should declare all globals with a var", function () {
         var context = {
                 global: global
@@ -28,15 +29,20 @@ describe("getImportGlobalsSrc", function () {
         delete global['__core-js_shared__'];
         delete global['a-b'];
 
-        expectedGlobals = Object.keys(global);
+        const ignoredGlobals = ["module", "exports", "require", "undefined", "eval", "arguments", "GLOBAL", "root", "NaN", "Infinity"];
+
+        const globals = Object.getOwnPropertyNames(global);
+        expectedGlobals = globals.filter((el) => !ignoredGlobals.includes(el));
 
         vm.runInNewContext(src, context);
-        actualGlobals = Object.keys(context);
+        actualGlobals = Object.getOwnPropertyNames(context);
+
         actualGlobals.sort();
         expectedGlobals.sort();
         expect(actualGlobals).to.eql(expectedGlobals);
         expect(actualGlobals.length).to.be.above(1);
     });
+
     it("should ignore the given variables", function () {
         var context = {
                 global: global
@@ -44,18 +50,22 @@ describe("getImportGlobalsSrc", function () {
             ignore = ["console", "setTimeout"],
             src,
             actualGlobals,
-            expectedGlobals = Object.keys(global);
+            expectedGlobals = Object.getOwnPropertyNames(global);
+
+        const ignoredGlobals = ["module", "exports", "require", "undefined", "eval", "arguments", "GLOBAL", "root", "NaN", "Infinity"];
+        ignore = ignore.concat(ignoredGlobals);
 
         // getImportGlobalsSrc modifies the ignore array, so let's create a copy
         src = getImportGlobalsSrc(ignore.slice(0));
-        expectedGlobals = expectedGlobals.filter(function filterIgnoredVars(value) {
-            return ignore.indexOf(value) === -1;
-        });
+        expectedGlobals = expectedGlobals.filter((el) => !ignore.includes(el));
+
         vm.runInNewContext(src, context);
         actualGlobals = Object.keys(context);
+
         actualGlobals.sort();
         expectedGlobals.sort();
         expect(actualGlobals).to.eql(expectedGlobals);
         expect(actualGlobals.length).to.be.above(1);
     });
+
 });
